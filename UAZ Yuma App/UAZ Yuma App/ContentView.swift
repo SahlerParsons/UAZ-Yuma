e462a5e8b98441c1a9449e799458505e9ebb8c8b
SahlerParsons <49286569+SahlerParsons@users.noreply.github.com>
2020-09-18 10:59:35 -0700

    Add files via upload

---
+++ UAZ Yuma App/UAZ Yuma App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UAZ Yuma App
+//
+//  Created by James Parsons on 4/16/20.
+//  Copyright © 2020 UAZ Yuma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UAZ Yuma App/UAZ Yuma AppTests/UAZ_Yuma_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  UAZ_Yuma_AppTests.swift
+//  UAZ Yuma AppTests
+//
+//  Created by James Parsons on 4/16/20.
+//  Copyright © 2020 UAZ Yuma. All rights reserved.
+//
+
+import XCTest
+@testable import UAZ_Yuma_App
+
+class UAZ_Yuma_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
